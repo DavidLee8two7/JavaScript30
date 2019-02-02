3500d04b80643c19276529c0af988201babb619e
David Lee <DavidLee8two7@gmail.com>
2019-02-01 22:09:25 -0800

    palindrom 2 solutions

--- reverse_str/index.js
+++ reverse_str/index.js
@@ -5,27 +5,19 @@
 //     .join("");
 // }
 
-/*
-split make string into array and join puts it back to str
-*/
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//     debugger;
+//   }
+//   return reversed;
+// }
 
 function reverse(str) {
-  let reversed = "";
-
-  for (let character of str) {
-    reversed = character + reversed;
-    debugger;
-  }
-  return reversed;
+  return str.split("").reduce((a, b) => b + a, "");
 }
-
-/*
-for (of) is better than reg. for loop syntax makes it less mistake
-*/
-
-// function reverse(str) {
-//   return str.split("").reduce((a, b) => b + a, "");
-// }
 reverse("abcd");
 
 module.exports = reverse;
